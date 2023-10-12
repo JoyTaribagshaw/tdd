86be6e07b1edeb33ce98ef7e92e1f9a2bc32e671
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-12 22:42:02 +0100

    added test that fails for fizz buzz

---
+++ specs/fizzbuzz_test.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative 'solver'
+
+describe Solver do
+describe '.fizzbuzz' do
+  it 'returns "fizz" for multiples of 3' do
+    expect(Solver.fizzbuzz(9)).to eq('fizz')
+  end
+end
+end
