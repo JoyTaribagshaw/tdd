e650f404d3749bc8306205d18edb63a8bcd6bcf1
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-12 22:56:15 +0100

    added another test that fails

--- specs/fizzbuzz_test.rb
+++ specs/fizzbuzz_test.rb
@@ -6,5 +6,9 @@ describe '.fizzbuzz' do
   it 'returns "fizz" for multiples of 3' do
     expect(Solver.fizzbuzz(9)).to eq('fizz')
   end
+
+it 'returns "buzz" for multiples of 5' do
+  expect(Solver.fizzbuzz(10)).to eq('buzz')
+end
 end
 end
