a03223eea7c50ad12e15fcb29826a00a7ad1321b
agneta1991 <pupienytea@gmail.com>
2023-10-13 09:44:42 +0300

    typos & linters fixed

--- specs/reverse_test.rb
+++ specs/reverse_test.rb
@@ -1,18 +1,10 @@
 require 'rspec'
 require_relative 'solver'
 
-class Solver
-  def self.factorial(num)
-    if num.zero?
-      1
-    elsif num.negative?
-      raise ArgumentError, 'number has to be positive or 0'
-    else
-      (1..num).reduce(:*)
+describe Solver do
+  describe '.reverse' do
+    it 'reverses a string' do
+      expect(Solver.reverse('reverse')).to eq('esrever')
     end
-  end
-
-  def self.reverse(word)
-    word.reverse
   end
 end
